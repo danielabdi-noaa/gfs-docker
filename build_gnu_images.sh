ebc47335c665872391088e2cbff7890138e217f6
Daniel Shawul <dshawul@yahoo.com>
2020-06-12 20:27:56 -0600

    Temporary fix for jenkins intel.

--- build_gnu_images.sh
+++ build_gnu_images.sh
@@ -12,25 +12,25 @@ build_image() {
     docker build --build-arg REPO=${REPO} -t ${1} -f ${2} .
 }
 
-#netcdf
-IMAGE_NAME=${REPO}/netcdf-gnu
-DOCKER_FILE=Dockerfiles/gnu/Dockerfile-netcdf
-build_image ${IMAGE_NAME} ${DOCKER_FILE}
-
-#esmf
-IMAGE_NAME=${REPO}/esmf-gnu
-DOCKER_FILE=Dockerfiles/gnu/Dockerfile-esmf
-build_image ${IMAGE_NAME} ${DOCKER_FILE}
-
-#fv3
-IMAGE_NAME=${REPO}/fv3-gnu
-DOCKER_FILE=Dockerfiles/gnu/Dockerfile-fv3
-build_image ${IMAGE_NAME} ${DOCKER_FILE}
-
-#nceplibs
-IMAGE_NAME=${REPO}/nceplibs-gnu
-DOCKER_FILE=Dockerfiles/gnu/Dockerfile-nceplibs
-build_image ${IMAGE_NAME} ${DOCKER_FILE}
+##netcdf
+#IMAGE_NAME=${REPO}/netcdf-gnu
+#DOCKER_FILE=Dockerfiles/gnu/Dockerfile-netcdf
+#build_image ${IMAGE_NAME} ${DOCKER_FILE}
+#
+##esmf
+#IMAGE_NAME=${REPO}/esmf-gnu
+#DOCKER_FILE=Dockerfiles/gnu/Dockerfile-esmf
+#build_image ${IMAGE_NAME} ${DOCKER_FILE}
+#
+##fv3
+#IMAGE_NAME=${REPO}/fv3-gnu
+#DOCKER_FILE=Dockerfiles/gnu/Dockerfile-fv3
+#build_image ${IMAGE_NAME} ${DOCKER_FILE}
+#
+##nceplibs
+#IMAGE_NAME=${REPO}/nceplibs-gnu
+#DOCKER_FILE=Dockerfiles/gnu/Dockerfile-nceplibs
+#build_image ${IMAGE_NAME} ${DOCKER_FILE}
 
 #gfs
 IMAGE_NAME=${REPO}/gfs-gnu
